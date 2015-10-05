50ffd66f305c3caf7bc87174071731ac202c9aa1
SeregaGomen <gserega71@gmail.com>
2015-10-05 14:34:46 +0300

    Last

--- tabledialog.cpp
+++ tabledialog.cpp
@@ -132,10 +132,6 @@ void TableDialog::setupDialog(void)
     ui->tb5->setEnabled(false);
     ui->tb6->setEnabled(false);
 
-//    if (!query.exec(QString("SELECT tbl_student.f_name,tbl_student.f_group,tbl_student.f_class,tbl_results.f_dt,f_res1,f_legend1,f_res2,f_legend2,f_res3,f_legend3,f_res4,f_legend4,f_res5,f_legend5,f_res6,f_legend6,tbl_results.id,f_id1,f_id2,f_id3,f_id4,f_id5,f_id6  \
-//                             FROM tbl_results,tbl_student,tbl_test1,tbl_test2,tbl_test3,tbl_test4,tbl_test5,tbl_test6 \
-//                             WHERE tbl_test1.id = tbl_results.f_id1 AND tbl_test2.id = tbl_results.f_id2 AND tbl_test3.id = tbl_results.f_id3 AND tbl_test4.id = tbl_results.f_id4 AND tbl_test5.id = tbl_results.f_id5 AND tbl_test6.id = tbl_results.f_id6 AND tbl_student.id = tbl_results.f_student \
-//                             ORDER BY tbl_results.f_dt,tbl_student.f_group,tbl_student.f_name")))
     if (!query.exec(QString("SELECT f_name,f_group,f_class,f_dt,f_res1,f_legend1,f_res2,f_legend2,f_res3,f_legend3,f_res4,f_legend4,f_res5,f_legend5,f_res6,f_legend6,f_student,tbl_results.id \
                              FROM tbl_results,tbl_student \
                              WHERE  tbl_student.id = tbl_results.f_student ")))
@@ -162,7 +158,7 @@ void TableDialog::setupDialog(void)
             ui->tableWidget->setItem(i, 7, new QTableWidgetItem(QString("%1 (%2)").arg(query.value(11).toString()).arg(query.value(10).toString()))); // Тест4
         if (!query.value(13).isNull())
             ui->tableWidget->setItem(i, 8, new QTableWidgetItem(QString("%1 (%2)").arg(query.value(13).toString()).arg(query.value(12).toString()))); // Тест5
-        if (!query.value(16).isNull())
+        if (!query.value(15).isNull())
             ui->tableWidget->setItem(i, 9, new QTableWidgetItem(QString("%1 (%2)").arg(query.value(15).toString()).arg(query.value(14).toString()))); // Тест6
         ui->tableWidget->setItem(i, 10, new QTableWidgetItem(query.value(16).toString()));  // Id студента
         ui->tableWidget->setItem(i, 11, new QTableWidgetItem(query.value(17).toString()));  // Id
